9d1b546ab6a76280652a701b201dec9fb787d368
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-11-28 13:12:25 +0200

    Need more memory

--- train_drugs_bcn.sh
+++ train_drugs_bcn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --mem=20G
+#SBATCH --mem=40G
 #SBATCH --time=1-0
 
 python run.py train experiments/drugs_bcn_without_cuda.json  --include-package drugs.dataset_readers --include-package drugs.models -s models/drugs_bcn
--- train_drugs_bcn_ebay_vs_onion_delex.sh
+++ train_drugs_bcn_ebay_vs_onion_delex.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --mem=20G
+#SBATCH --mem=30G
 #SBATCH --time=1-0
 
 python run.py train experiments/drugs_bcn_ebay_vs_onion_delex_without_cuda.json  --include-package drugs.dataset_readers --include-package drugs.models -s models/drugs_bcn_delex
