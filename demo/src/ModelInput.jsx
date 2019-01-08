0c914e20e65af284ecf3197f449f1ebeac38e0b5
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-01-08 15:11:59 +0200

    Customize demo ModelInput

--- demo/src/ModelInput.jsx
+++ demo/src/ModelInput.jsx
@@ -3,37 +3,36 @@ import Button from './components/Button'
 import ModelIntro from './components/ModelIntro'
 
 
-// TODO: These are some quickly-accessible examples to try out with your model.  They will get
+// These are some quickly-accessible examples to try out with your model.  They will get
 // added to the select box on the demo page, and will auto-populate your input fields when they
 // are selected.  The names here need to match what's read in `handleListChange` below.
 
 const examples = [
   {
-    long_text_input: "long text input for example 1",
-    short_text_input: "short text input for example 1"
+    label: "ebay example",
+    text_input: "Due to the difference between different monitors, the picture may not reflect the actual color of the item.",
   },
   {
-    long_text_input: "long text input for example 2",
-    short_text_input: "short text input for example 2"
+    label: "onion illegal example",
+    text_input: "Product Price Quantity Pack of 10x1cc BD Insulin Syringes 10 USD 0.015 Bh. SAMPLER! One Point Of Heroin 4 (0.10g) 30 USD 0.046 Bh. GRAND OPENING SPECIAL QUATER GRAM HEROIN 4 (0.25g) 55 USD 0.084 Bh. HALF GRAM HEROIN 4 (0.50g) 100 USD 0.153 Bh. GRAND OPENING SPECIAL FULL WEIGHED GRAM HEROIN 4 (1.0g) 180 USD",
   },
   {
-    long_text_input: "long text input for example 3",
-    short_text_input: "short text input for example 3"
+    label: "onion legal example",
+    text_input: "Betnovate Betamethasone valerate Scalp application (lotion) 30 ml US$29.00 each, manufactured by: GlaxoSmithKline Identical to Beta-Val Betamethasone valerate Lotion 30 ml",
   }
 ];
 
-// TODO: This determines what text shows up in the select box for each example.  The input to
+// This determines what text shows up in the select box for each example.  The input to
 // this function will be one of the items from the `examples` list above.
 function summarizeExample(example) {
-  return example.long_text_input.substring(0, 60);
+  return example.label;
 }
 
-// TODO: You can give a model name and description that show up in your demo.
-const title = "Your Model Name";
+// You can give a model name and description that show up in your demo.
+const title = "Cyber Classification Demo";
 const description = (
   <span>
-  If you want a description of what this demo is showing, you can put that here.  Or just leave this
-  description empty if you don't need it.
+  Classify a paragraph as legal/illegal.
   </span>
 );
 
@@ -46,19 +45,18 @@ class ModelInput extends React.Component {
 
   handleListChange(e) {
     if (e.target.value !== "") {
-      // TODO: This gets called when the select box gets changed.  You want to set the values of
+      // This gets called when the select box gets changed.  You want to set the values of
       // your input boxes with the content in your examples.
-      this.long_text_input.value = examples[e.target.value].long_text_input
-      this.short_text_input.value = examples[e.target.value].short_text_input
+      this.text_input.value = examples[e.target.value].text_input
     }
   }
 
   onClick() {
     const { runModel } = this.props;
 
-    // TODO: You need to map the values in your input boxes to json values that get sent to your
+    // You need to map the values in your input boxes to json values that get sent to your
     // predictor.  The keys in this dictionary need to match what your predictor is expecting to receive.
-    runModel({long_text_input: this.long_text_input.value, short_text_input: this.short_text_input.value});
+    runModel({text_input: this.text_input.value});
   }
 
   render() {
@@ -73,14 +71,14 @@ class ModelInput extends React.Component {
               <option value="">Choose an example...</option>
               {examples.map((example, index) => {
                 return (
-                    <option value={index} key={index}>{summarizeExample(example) + "..."}</option>
+                    <option value={index} key={index}>{summarizeExample(example)}</option>
                 );
               })}
           </select>
         </div>
 
        {/*
-         * TODO: This is where you add your input fields.  You shouldn't have to change any of the
+         * This is where you add your input fields.  You shouldn't have to change any of the
          * code in render() above here.  We're giving a couple of example inputs here, one for a
          * larger piece of text, like a paragraph (the `textarea`) and one for a shorter piece of
          * text, like a question (the `input`).  You'll probably want to change the variable names
@@ -88,13 +86,13 @@ class ModelInput extends React.Component {
          */}
 
         <div className="form__field">
-          <label>Long text input</label>
-          <textarea ref={(x) => this.long_text_input = x} type="text" autoFocus="true"></textarea>
+          <label>Text input</label>
+          <textarea ref={(x) => this.text_input = x} type="text" autoFocus="true"></textarea>
         </div>
-        <div className="form__field">
-          <label>Short text input</label>
-          <input ref={(x) => this.short_text_input = x} type="text"/>
-        </div>
+        {/*<div className="form__field">*/}
+          {/*<label>Short text input</label>*/}
+          {/*<input ref={(x) => this.short_text_input = x} type="text"/>*/}
+        {/*</div>*/}
 
        {/* You also shouldn't have to change anything below here. */}
 
