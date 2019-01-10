d8dd5e8b85af11de016814a6af4538565cf7621f
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-01-10 17:30:19 +0200

    Cumulative attention weight reports

--- cyber/metrics/attention.py
+++ cyber/metrics/attention.py
@@ -25,12 +25,16 @@ class AttentionMetric(Metric):
                                                                            repeat(None) if mask is None else mask):
             for token, weight, token_mask in zip(instance_tokens, instance_weights, instance_mask):
                 if mask is None or token_mask:
-                    key = " ".join((label, token))
+                    key = "\t".join((label, token))
                     self._cumulative_attention[key] += weight.item()
                     self._occurrences[key] += 1
 
     @overrides
     def get_metric(self, reset: bool = False):
+        if self._occurrences:
+            with open("attention.tsv", "w", encoding="utf-8") as f:
+                for word, occurrences in self._occurrences.most_common():
+                    print(word, occurrences, self._cumulative_attention[word], sep="\t", file=f)
         average_attention = Counter(**{k: v / self._occurrences[k] for k, v in self._cumulative_attention.items()})
         if reset:
             self.reset()
