18f9f826a6c90f5d9cda6b420b520f26d2d71e70
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-01-27 09:31:14 +0200

    Rename f1.txt to t1.tsv

--- train_all.sh
+++ train_all.sh
@@ -15,6 +15,6 @@ for JSON in ${JSONS[@]}; do
     mkdir -p "models/${SETTING}"
     MODEL="models/${SETTING}/${EXPERIMENT}"
     python run.py train "${JSON}" --include-package cyber.dataset_readers --include-package cyber.models -s "${MODEL}" \
-        || grep Error "${MODEL}/stderr.log" >> f1.txt
-    echo -e "${SETTING}\t${EXPERIMENT}\t$(grep -h test_f1 ${MODEL}/stdout.log | sed 's/.*://;s/,//;s/ //g')" >> f1.txt
+        || grep Error "${MODEL}/stderr.log" >> f1.tsv
+    echo -e "${SETTING}\t${EXPERIMENT}\t$(grep -h test_f1 ${MODEL}/stdout.log | sed 's/.*://;s/,//;s/ //g')" >> f1.tsv
 done
