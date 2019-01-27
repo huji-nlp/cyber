12356ce287c20457158af1979c8f882d2a759486
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-01-27 09:18:08 +0200

    Print setting to f1.txt

--- train_all.sh
+++ train_all.sh
@@ -15,5 +15,5 @@ for JSON in ${JSONS[@]}; do
     mkdir -p "models/${SETTING}"
     MODEL="models/${SETTING}/${EXPERIMENT}"
     python run.py train "${JSON}" --include-package cyber.dataset_readers --include-package cyber.models -s "${MODEL}"
-    echo "${EXPERIMENT}$(grep -h test_f1 ${MODEL}/stdout.log | sed 's/.*://;s/,//')" >> f1.txt
+    echo -e "${SETTING}\t${EXPERIMENT}\t$(grep -h test_f1 ${MODEL}/stdout.log | sed 's/.*://;s/,//;s/ //g')" >> f1.txt
 done
