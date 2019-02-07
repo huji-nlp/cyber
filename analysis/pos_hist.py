450c166a340640714e22d8e94d66451f1fa69e10
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-07 10:08:18 +0200

    Nicer POS hist table

--- analysis/pos_hist.py
+++ analysis/pos_hist.py
@@ -7,8 +7,15 @@ DATA_DIR = "data"
 DATA_FILES = [os.path.join(DATA_DIR, f) for f in os.listdir(DATA_DIR) if f.endswith(".txt")]
 
 nlp = spacy.load("en")
-
+all_pos = set()
+hists = {}
 for data_file in DATA_FILES:
     with open(data_file, encoding="utf-8") as f:
         pos_hist = Counter(token.pos_ for doc in nlp.pipe(f) for token in doc)
-    print(data_file, *pos_hist.most_common(), sep="\t")
+    all_pos.update(pos_hist)
+    hists[data_file] = pos_hist
+
+all_pos = sorted(all_pos)
+print("", *all_pos, sep="\t")
+for data_file, pos_hist in sorted(hists.items()):
+    print(os.path.basename(os.path.splitext(data_file)[0]), *[pos_hist[p] for p in all_pos], sep="\t")
