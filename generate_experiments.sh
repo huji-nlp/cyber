b44e1fe6b7a2b04a26258ecb3d4adf41ff94312d
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-18 14:51:49 +0200

    Cross-domain experiments

--- generate_experiments.sh
+++ generate_experiments.sh
@@ -7,4 +7,7 @@ for EXPERIMENT in experiments/ebay_vs_onion_drugs_legal/*; do
     mkdir -p ${OUTDIR}
     sed "s/onion_drugs_legal/onion_${PART}_illegal/;s/ebay/onion_${PART}_legal/" ${EXPERIMENT} > ${OUTDIR}/${BASENAME}
   done
+  OUTDIR=experiments/onion_drugs_legal_vs_illegal_test_forums
+  mkdir -p ${OUTDIR}
+  sed "/test/s/drugs/forums/" experiments/onion_drugs_legal_vs_illegal/${BASENAME} > ${OUTDIR}/${BASENAME}
 done
