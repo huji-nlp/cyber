cbd46c465a9aac7214c4992a5f9063030362e0e1
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-18 16:43:41 +0200

    Remove now-unused code for masking input while loading it

--- cyber/dataset_readers/fetch_documents.py
+++ cyber/dataset_readers/fetch_documents.py
@@ -16,13 +16,10 @@ CATEGORIES = ("ebay", "illegal", "legal")
 
 @DatasetReader.register("document")
 class DocumentDatasetReader(DatasetReader):
-    def __init__(self, tokenizer: Tokenizer = None, token_indexers: Optional[Dict[str, TokenIndexer]] = None,
-                 mask: Optional[Tuple[str]] = None, drop_masked: bool = False) -> None:
+    def __init__(self, tokenizer: Tokenizer = None, token_indexers: Optional[Dict[str, TokenIndexer]] = None) -> None:
         super().__init__()
         self._tokenizer = tokenizer or WordTokenizer()
         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
-        self._mask = mask
-        self._drop_masked = drop_masked
 
     @overrides
     def _read(self, file_paths):
@@ -33,10 +30,8 @@ class DocumentDatasetReader(DatasetReader):
     @overrides
     def text_to_instance(self, text: str, target: int = None) -> Instance:
         tokens = self._tokenizer.tokenize(text)
-        masked_tokens = list(filter(None, (self.mask_token(token) for token in tokens)))
-        text_field = TextField(masked_tokens, self._token_indexers)
-        metadata = {"tokens": [token.text for token in tokens],
-                    "masked_tokens": [token.text for token in masked_tokens]}
+        text_field = TextField(tokens, self._token_indexers)
+        metadata = {"tokens": [token.text for token in tokens]}
         fields: Dict[str, Field] = {"text": text_field}
         if target is not None:
             fields["label"] = LabelField(target)
@@ -50,11 +45,3 @@ class DocumentDatasetReader(DatasetReader):
             with open(file_path, encoding="utf-8") as f:
                 for line in f:
                     yield line, category
-
-    def mask_token(self, token):
-        if self._mask is None or token.pos_ not in self._mask:
-            return token
-        if self._drop_masked:
-            return None
-        return Token(text=token.pos_, idx=token.idx, lemma=token.pos_, pos=token.pos_, tag=token.tag_, dep=token.dep_,
-                     ent_type=token.ent_type_)
