b291af5c8cf104fa0b2242763d6fdd983abc6992
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-18 14:19:43 +0200

    Specify file path instead of category and split in experiment configuration

--- cyber/dataset_readers/fetch_documents.py
+++ cyber/dataset_readers/fetch_documents.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from typing import Dict, Tuple, Optional
 
 from allennlp.data import Token
@@ -10,31 +9,26 @@ from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 from allennlp.data.tokenizers import Tokenizer, WordTokenizer
 from overrides import overrides
 
-from cyber.util.clean_text import read_file, clean_lines
-from cyber.util.split_data import DATA_SUBDIRS, clean_file_path
-
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+CATEGORIES = ("ebay", "illegal", "legal")
 
 
 @DatasetReader.register("document")
 class DocumentDatasetReader(DatasetReader):
     def __init__(self, tokenizer: Tokenizer = None, token_indexers: Optional[Dict[str, TokenIndexer]] = None,
-                 categories: Optional[Tuple[str]] = None, mask: Optional[Tuple[str]] = None,
-                 drop_masked: bool = False) -> None:
+                 mask: Optional[Tuple[str]] = None, drop_masked: bool = False) -> None:
         super().__init__()
         self._tokenizer = tokenizer or WordTokenizer()
         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
-        self._categories = [c.split(os.sep) for c in categories] or DATA_SUBDIRS
         self._mask = mask
         self._drop_masked = drop_masked
 
     @overrides
-    def _read(self, file_path):
-        logger.info("Reading %s instance(s)", file_path)
-        drugs_data = self.fetch_documents(subset=file_path, categories=self._categories)\
-            if file_path in ("train", "validation", "test") else [(l, None) for l in clean_lines(read_file(file_path))]
-        for text, target in drugs_data:
-            yield self.text_to_instance(text, target)
+    def _read(self, file_paths):
+        logger.info("Reading instances from " + ", ".join(file_paths))
+        for line, category in self.fetch_documents(file_paths):
+            yield self.text_to_instance(line, category)
 
     @overrides
     def text_to_instance(self, text: str, target: int = None) -> Instance:
@@ -50,11 +44,12 @@ class DocumentDatasetReader(DatasetReader):
         return Instance(fields)
 
     @staticmethod
-    def fetch_documents(subset, categories):
-        for category in categories:
-            with open(clean_file_path(subdir=category, div=subset), encoding="utf-8") as f:
+    def fetch_documents(file_paths):
+        for file_path in file_paths:
+            category = next((c for c in CATEGORIES if c in file_path), None)
+            with open(file_path, encoding="utf-8") as f:
                 for line in f:
-                    yield line, "/".join(category)
+                    yield line, category
 
     def mask_token(self, token):
         if self._mask is None or token.pos_ not in self._mask:
