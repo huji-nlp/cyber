cbf33a4730633231f6f67f9ab293e9dbc86a4426
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-18 14:54:00 +0200

    Update list of experiments

--- generate_experiments.sh
+++ generate_experiments.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+rm -f experiments.txt
 for EXPERIMENT in experiments/ebay_vs_onion_drugs_legal/*; do
+  echo ${EXPERIMENT} >> experiments.txt
   BASENAME=$(basename ${EXPERIMENT})
   for PART in drugs forums; do
     OUTDIR=experiments/onion_${PART}_legal_vs_illegal
     mkdir -p ${OUTDIR}
     sed "s/onion_drugs_legal/onion_${PART}_illegal/;s/ebay/onion_${PART}_legal/" ${EXPERIMENT} > ${OUTDIR}/${BASENAME}
+    echo ${OUTDIR}/${BASENAME} >> experiments.txt
   done
   OUTDIR=experiments/onion_drugs_legal_vs_illegal_test_forums
   mkdir -p ${OUTDIR}
   sed "/test/s/drugs/forums/" experiments/onion_drugs_legal_vs_illegal/${BASENAME} > ${OUTDIR}/${BASENAME}
+  echo ${OUTDIR}/${BASENAME} >> experiments.txt
 done
