e92dd01885065b5379b4255914c3965f4394dcfe
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-18 16:17:46 +0200

    Read manipulated input from preprocessed files instead of online masking

--- generate_experiments.sh
+++ generate_experiments.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
+SOURCE_DIR=experiments/ebay_vs_onion_drugs_legal
+for MODEL in elmoattention gloveboe_averaged gloveboe_sum gloveseq2vec naivebayes; do
+  EXPERIMENT=${SOURCE_DIR}/${MODEL}.json
+  for MASK in dropcontent dropfunc poscontent posfunc; do
+    sed "s/\(data\S*\)\(\.txt\)/\1.${MASK}\2/g" ${EXPERIMENT} > ${SOURCE_DIR}/${MODEL}_${MASK}.json
+  done
+done
+
 rm -f experiments.txt
-for EXPERIMENT in experiments/ebay_vs_onion_drugs_legal/*; do
+for EXPERIMENT in ${SOURCE_DIR}/*; do
   echo ${EXPERIMENT} >> experiments.txt
   BASENAME=$(basename ${EXPERIMENT})
   for PART in drugs forums; do
-    OUTDIR=experiments/onion_${PART}_legal_vs_illegal
-    mkdir -p ${OUTDIR}
-    sed "s/onion_drugs_legal/onion_${PART}_illegal/;s/ebay/onion_${PART}_legal/" ${EXPERIMENT} > ${OUTDIR}/${BASENAME}
-    echo ${OUTDIR}/${BASENAME} >> experiments.txt
+    OUT_DIR=experiments/onion_${PART}_legal_vs_illegal
+    mkdir -p ${OUT_DIR}
+    sed "s/onion_drugs_legal/onion_${PART}_illegal/;s/ebay/onion_${PART}_legal/" ${EXPERIMENT} > ${OUT_DIR}/${BASENAME}
+    echo ${OUT_DIR}/${BASENAME} >> experiments.txt
   done
-  OUTDIR=experiments/onion_drugs_legal_vs_illegal_test_forums
-  mkdir -p ${OUTDIR}
-  sed "/test/s/drugs/forums/" experiments/onion_drugs_legal_vs_illegal/${BASENAME} > ${OUTDIR}/${BASENAME}
-  echo ${OUTDIR}/${BASENAME} >> experiments.txt
+  OUT_DIR=experiments/onion_drugs_legal_vs_illegal_test_forums
+  mkdir -p ${OUT_DIR}
+  sed "/test/s/drugs/forums/" experiments/onion_drugs_legal_vs_illegal/${BASENAME} > ${OUT_DIR}/${BASENAME}
+  echo ${OUT_DIR}/${BASENAME} >> experiments.txt
 done
 NUM_EXPERIMENTS=$(($(cat experiments.txt | wc -l) - 1))
 sed -i "s/--array=0-.*/--array=0-${NUM_EXPERIMENTS}/" train_all.sh
--- train_all.sh
+++ train_all.sh
@@ -2,7 +2,7 @@
 #SBATCH --mem=15G
 #SBATCH --time=0-6
 #SBATCH --gres=gpu:1
-#SBATCH --array=0-99
+#SBATCH --array=0-131
 
 JSONS=($(cat experiments.txt))
 if [[ -n "${SLURM_ARRAY_TASK_ID}" ]]; then
