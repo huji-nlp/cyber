f16a0142d462c5a0f9cf41e1b4b68dd1c4cb336e
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-25 10:40:22 +0100

    Add SVM linear

--- train_all.sh
+++ train_all.sh
@@ -2,7 +2,7 @@
 #SBATCH --mem=15G
 #SBATCH --time=0-6
 #SBATCH --gres=gpu:1
-#SBATCH --array=0-143
+#SBATCH --array=0-167
 
 JSONS=($(cat experiments.txt))
 if [[ -n "${SLURM_ARRAY_TASK_ID}" ]]; then
