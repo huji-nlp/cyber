f7f6e30ba9a6fb3f0fc04a4f9bd66ff6fdf619ee
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-02-25 11:42:36 +0200

    Fix output files

--- train_all.sh
+++ train_all.sh
@@ -15,6 +15,6 @@ for JSON in ${JSONS[@]}; do
     mkdir -p "models/${SETTING}"
     MODEL="models/${SETTING}/${EXPERIMENT}"
     python run.py train "${JSON}" --include-package cyber.dataset_readers --include-package cyber.models -s "${MODEL}" \
-        || grep Error "${MODEL}/stderr.log" >> ${SETTING}_${EXPERIMENT}_accuracy.tsv
-    grep -h test_accuracy ${MODEL}/stdout.log | sed 's/.*://;s/,//;s/ //g' >> ${SETTING}_${EXPERIMENT}_accuracy.tsv
+        || grep Error "${MODEL}/stderr.log" >> ${SETTING}_accuracy.tsv
+    grep -h test_accuracy ${MODEL}/stdout.log | sed 's/.*://;s/,//;s/ //g' >> ${SETTING}_accuracy.tsv
 done
